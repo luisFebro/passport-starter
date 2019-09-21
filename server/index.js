91e88483c6e9d5980b1a0f24a54ab74d786bf946
Luis Febro <mr.febro@gmail.com>
2019-09-20 20:40:52 -0400

    finishing the project

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const passport = require("passport");
-const FacebookStrategy = require("passport-facebook").Strategy;
-const GithubStrategy = require("passport-github").Strategy;
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const InstagramStrategy = require("passport-instagram").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const GithubStrategy = require("passport-github").Strategy;
+const AmazonStrategy = require("passport-amazon").Strategy;
 const SpotifyStrategy = require("passport-spotify").Strategy;
 const TwitchStrategy = require("passport-twitch.js").Strategy;
-const SpotifyStrategy = require("passport-spotify").Strategy;
-const keys = require("..config");
+const keys = require("../config");
 const chalk = require("chalk");
 let user = {};
 
@@ -20,153 +20,148 @@ passport.deserializeUser((user, cb) => {
     cb(null, user);
 });
 
-const app = express();
-app.use(cors());
-app.use(passport.initialize());
-
 // Google Strategy
 passport.use(new GoogleStrategy({
         clientID: keys.GOOGLE.clientID,
         clientSecret: keys.GOOGLE.clientSecret,
-        callbackUrl: "/auth/google/callback"
+        callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, cb) => {
         console.log(chalk.blue(JSON.stringify(profile)));
         user = { ...profile };
         return cb(null, profile);
     }));
-
-app.get("/auth/google", passport.authenticate(strategy: "google", options: {
-    scope: ["profile", "email"]
-}));
-app.get("/auth/google/callback",
-    passport.autheticate("google"),
-    options: (req, res) => {
-        res.redirect("/profile");
-    });
 // End Google Strategy
 
 // Facebook Strategy
 passport.use(new FacebookStrategy({
         clientID: keys.FACEBOOK.clientID,
         clientSecret: keys.FACEBOOK.clientSecret,
-        callbackUrl: "/auth/facebook/callback"
+        callbackURL: "/auth/facebook/callback"
     },
     (accessToken, refreshToken, profile, cb) => {
         console.log(chalk.blue(JSON.stringify(profile)));
         user = { ...profile };
         return cb(null, profile);
     }));
-
-app.get("/auth/facebook", passport.authenticate(strategy: "facebook"));
-app.get("/auth/facebook/callback",
-    passport.autheticate("facebook"),
-    options: (req, res) => {
-        res.redirect("/profile");
-    });
 // End Facebook Strategy
 
 // Instagram Strategy
 passport.use(new InstagramStrategy({
         clientID: keys.INSTAGRAM.clientID,
         clientSecret: keys.INSTAGRAM.clientSecret,
-        callbackUrl: "/auth/instagram/callback"
+        callbackURL: "/auth/instagram/callback"
     },
     (accessToken, refreshToken, profile, cb) => {
         console.log(chalk.blue(JSON.stringify(profile)));
         user = { ...profile };
         return cb(null, profile);
     }));
-
-app.get("/auth/instagram", passport.authenticate(strategy: "instagram"));
-app.get("/auth/instagram/callback",
-    passport.autheticate("instagram"),
-    options: (req, res) => {
-        res.redirect("/profile");
-    });
 // End Instagram Strategy
 
 // Amazon Strategy
 passport.use(new AmazonStrategy({
         clientID: keys.AMAZON.clientID,
         clientSecret: keys.AMAZON.clientSecret,
-        callbackUrl: "/auth/amazon/callback"
+        callbackURL: "/auth/amazon/callback"
     },
     (accessToken, refreshToken, profile, cb) => {
         console.log(chalk.blue(JSON.stringify(profile)));
         user = { ...profile };
         return cb(null, profile);
     }));
-
-app.get("/auth/amazon", passport.authenticate(strategy: "amazon", options: {
-    scope: ["profile"]
-}));
-app.get("/auth/amazon/callback",
-    passport.autheticate("amazon"),
-    options: (req, res) => {
-        res.redirect("/profile");
-    });
 // End Amazon Strategy
 
 // Github Strategy
 passport.use(new GithubStrategy({
         clientID: keys.GITHUB.clientID,
         clientSecret: keys.GITHUB.clientSecret,
-        callbackUrl: "/auth/github/callback"
+        callbackURL: "/auth/github/callback"
     },
     (accessToken, refreshToken, profile, cb) => {
         console.log(chalk.blue(JSON.stringify(profile)));
         user = { ...profile };
         return cb(null, profile);
     }));
-
-app.get("/auth/github", passport.authenticate(strategy: "github"));
-app.get("/auth/github/callback",
-    passport.autheticate("github"),
-    options: (req, res) => {
-        res.redirect("/profile");
-    });
 // End Github Strategy
 
 // Spotify Strategy
 passport.use(new SpotifyStrategy({
         clientID: keys.SPOTIFY.clientID,
         clientSecret: keys.SPOTIFY.clientSecret,
-        callbackUrl: "/auth/spotify/callback"
+        callbackURL: "/auth/spotify/callback"
     },
     (accessToken, refreshToken, profile, cb) => {
         console.log(chalk.blue(JSON.stringify(profile)));
         user = { ...profile };
         return cb(null, profile);
     }));
-
-app.get("/auth/spotify", passport.authenticate(strategy: "spotify"));
-app.get("/auth/spotify/callback",
-    passport.autheticate("spotify"),
-    options: (req, res) => {
-        res.redirect("/profile");
-    });
 // End Spotify Strategy
 
 // Twitch Strategy
 passport.use(new TwitchStrategy({
         clientID: keys.TWITCH.clientID,
         clientSecret: keys.TWITCH.clientSecret,
-        callbackUrl: "/auth/twitch/callback"
+        callbackURL: "/auth/twitch/callback"
     },
     (accessToken, refreshToken, profile, cb) => {
         console.log(chalk.blue(JSON.stringify(profile)));
         user = { ...profile };
         return cb(null, profile);
     }));
+// End Twitch Strategy
 
-app.get("/auth/twitch", passport.authenticate(strategy: "twitch"));
-app.get("/auth/twitch/callback",
-    passport.autheticate("twitch"),
-    options: (req, res) => {
+const app = express();
+app.use(cors());
+app.use(passport.initialize());
+
+app.get("/auth/google", passport.authenticate("google", {
+    scope: ["profile", "email"]
+}));
+app.get("/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
         res.redirect("/profile");
     });
-// End Twitch Strategy
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+app.get("/auth/facebook/callback",
+    passport.authenticate("facebook"),
+    (req, res) => {
+        res.redirect("/profile");
+    });
+
+app.get("/auth/instagram", passport.authenticate("instagram"));
+app.get("/auth/instagram/callback",
+    passport.authenticate("instagram"),
+    (req, res) => {
+        res.redirect("/profile");
+    });
+
+app.get("/auth/amazon", passport.authenticate("amazon", {
+    scope: ["profile"]
+}));
+
+app.get("/auth/github", passport.authenticate("github"));
+app.get("/auth/github/callback",
+    passport.authenticate("github"),
+    (req, res) => {
+        res.redirect("/profile");
+    });
+
+app.get("/auth/spotify", passport.authenticate("spotify"));
+app.get("/auth/spotify/callback",
+    passport.authenticate("spotify"),
+    (req, res) => {
+        res.redirect("/profile");
+    });
+
+app.get("/auth/twitch", passport.authenticate("twitch.js"));
+app.get("/auth/twitch/callback",
+    passport.authenticate("twitch.js"),
+    (req, res) => {
+        res.redirect("/profile");
+    });
 
 app.get("/user", (req, res) => {
     console.log("getting user data!");
